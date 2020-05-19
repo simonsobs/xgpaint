d72db1c9ae41ce44d581cb087429f658decb30f4
georgestein <george.f.stein@gmail.com>
2020-05-19 13:35:18 -0700

    newest scripts

--- halo2fluxmap/websky-cib/makemaps_cib.py
+++ halo2fluxmap/websky-cib/makemaps_cib.py
@@ -1,64 +1,18 @@
-#!/usr/bin/env python
-import sys
-import numpy             as np
-import healpy            as hp
-import matplotlib.pyplot as plt
-import halo2fluxmap      as h2fm
+#!/bin/bash -l
+#SBATCH -q premium
+#SBATCH -N 16
+#SBATCH --tasks-per-node=8
+#SBATCH -C haswell
+#SBATCH -A mp107
+#SBATCH -t 8:00:00
+#SBATCH -J h2fm
+#SBATCH -o h2fm.o%j
+#SBATCH -L SCRATCH,project
 
-import mpi4py.rc
-import datetime
-import os
-import psutil
+export OMP_PROC_BIND=true
+export OMP_PLACES=threads
+export OMP_NUM_THREADS=8
 
-from   scipy.ndimage.filters import gaussian_filter
-
-h2fm.init.initialize('param/h2fm_params.txt')
-
-if len(sys.argv) > 1:
-    h2fm.params.octx = int(sys.argv[1])
-    h2fm.params.octy = int(sys.argv[2])
-    h2fm.params.octz = int(sys.argv[3])
-    h2fm.params.outbase = h2fm.params.outbase+'-'+sys.argv[1]+sys.argv[2]+sys.argv[3]
-    print h2fm.params.octx,h2fm.params.octy,h2fm.params.octz,h2fm.params.outbase
-
-h2fm.utils.report('Reading, Shuffling, Culling Catalogue',2)
-
-# Get catalog of centrals
-cen = h2fm.io.read_catalog()
-
-# Get number of satellites for each halo
-ns, nsmean  = h2fm.hod.cen2ns(cen)
-
-# Populate halos with satellites
-sat = h2fm.hod.populate_centrals(cen,ns)
-
-# Write time
-h2fm.utils.write_time("HOD completed", h2fm.params.rank)
-
-#loop over frequencies
-for inu_map in range(len(h2fm.params.freq_list)):
-
-    # Put halos in map
-    h2fm.params.nu_obs     = h2fm.params.freq_list[inu_map] * 1.e9
-    h2fm.params.nu_obs_GHz = h2fm.params.freq_list[inu_map]
-        
-     # Give flux to gals and put in map
-#    intensity, flux_cen, flux_sat = h2fm.halos2map.halos2map(cen,ns,nsmean,sat) 
- 
-    # Give flux to gals and put in map
-    #    intensity, flux_cen, flux_sat = h2fm.halos2map.halos2map(cen,ns,nsmean,sat)
-    pcen,mcen,fcen,nsat,psat,msat,fsat,lcen,lsat = h2fm.halos2sources.halos2sources(cen,ns,nsmean,sat)
-    intensity, flux_cen, flux_sat                = h2fm.sources2map.sources2map(pcen,mcen,fcen,nsat,
-                                                                                psat,msat,fsat,lcen,lsat)       
-
-    # Write map
-    ns_str   = str(h2fm.params.nside)
-    nu_str   = str(int(h2fm.params.freq_list[inu_map])).zfill(4)
-    base = 'maps/'+h2fm.params.outbase 
-    base += ('_ns'+ns_str+'_nu'+nu_str)
-
-    h2fm.io.writemap(base,intensity)
-
-# Write time
-h2fm.utils.write_time("Halo projection completed",h2fm.params.rank)
-
+# run driver
+conda activate halo2fluxmap
+srun -n $SLURM_NTASKS --mpi=pmi2 python scripts/makemaps_cib_viero.py
